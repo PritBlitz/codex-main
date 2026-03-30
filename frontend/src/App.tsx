b960f1f36de83a4768578f688d375d09efbec16e
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2026-03-31 01:54:41 +0530

    Added favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import AmbientBackground from "./components/animations/AmbientBackground"
 
 // Inner component so useLocation works inside Router
 function AnimatedRoutes() {
+
   const location = useLocation()
   return (
     <>
